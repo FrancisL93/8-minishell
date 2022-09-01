0cb3deae05748ea29d15a8656afcd65185bc8905
TheOdd23 <100093373+TheOdd23@users.noreply.github.com>
2022-09-01 17:14:55 -0400

    Add files via upload

---
+++ minishell/src/built_in_tools.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_in_tools.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flahoud <flahoud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 11:39:20 by anhebert          #+#    #+#             */
+/*   Updated: 2022/08/31 12:59:29 by flahoud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+void	set_pwd(char *oldpath)
+{
+	int		i;
+	char	*buff;
+
+	buff = NULL;
+	i = 0;
+	while (environ[i] && ft_strncmp(environ[i], "PWD=", 4))
+		i++;
+	if (environ[i])
+		environ[i] = ft_strjoin("PWD=", getcwd(buff, 1024));
+	i = 0;
+	while (environ[i] && ft_strncmp(environ[i], "OLDPWD=", 7))
+		i++;
+	if (environ[i])
+		environ[i] = ft_strjoin("OLDPWD=", oldpath);
+}
+
+int	ftstrnstr(char *current_path, char *cmd)
+{
+	int	i;
+	int	ii;
+
+	i = -1;
+	ii = 0;
+	while (current_path[++i])
+	{
+		if (cmd[ii] != current_path[i])
+			return (0);
+		ii++;
+		if (cmd[ii] == '\0')
+			break ;
+	}
+	return (1);
+}
+
+char	*ftstrjoin(char *cmd, char *current_path)
+{
+	int		i;
+	int		j;
+	int		size;
+	char	*new_path;
+
+	i = -1;
+	j = 0;
+	size = ft_strlen(current_path);
+	size += ft_strlen(cmd);
+	new_path = calloc(sizeof(char), size + 2);
+	while (current_path[++i])
+		new_path[j++] = current_path[i];
+	new_path[j] = '/';
+	i = -1;
+	while (cmd[++i])
+		new_path[++j] = cmd[i];
+	return (new_path);
+}
+
+char	*ftstrtrim(char	*current_path)
+{
+	int		i;
+	int		ii;
+	int		j;
+	char	*new_path;
+
+	i = 0;
+	ii = 0;
+	j = 0;
+	new_path = calloc(sizeof(char), ft_strlen(current_path));
+	while (current_path[i])
+		i++;
+	while (current_path[i] != '/')
+		i--;
+	while (ii < i)
+	{
+		new_path[j] = current_path[ii];
+		j++;
+		ii++;
+	}
+	return (new_path);
+}
+
+char	*check_path(char *cmd, char *current_path)
+{
+	char	*buff;
+
+	buff = NULL;
+	if (cmd[0] == '.' && cmd[1] == '\0')
+		return (getcwd(buff, 1024));
+	else if (ft_strlen(current_path) == 1 && current_path[0] == '/')
+		return (current_path);
+	else if (cmd[0] == '.' && cmd[1] == '.' && cmd[2] == '\0')
+		return (ftstrtrim(current_path));
+	return (NULL);
+}
---
+++ minishell/src/exe.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exe.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhebert <anhebert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 13:17:40 by flahoud           #+#    #+#             */
+/*   Updated: 2022/09/01 13:08:21 by anhebert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+void	execute_built_in(t_vars *vars, char *input)
+{
+	if (!ft_strncmp(tolower_str(vars->cmd), "env", 3))
+		print_env();
+	if (!ft_strncmp(tolower_str(vars->cmd), "pwd", 3))
+		print_path();
+	if (!ft_strncmp(tolower_str(vars->cmd), "echo", 4))
+		echo(vars);
+	if (!ft_strncmp(tolower_str(vars->cmd), "cd", 2))
+		cd(input);
+	if (!ft_strncmp(tolower_str(vars->cmd), "export", 6))
+		export(vars, input);
+}
+
+//remplacer ft_split par lexer
+/* void	execute_cmd(char *input)
+{
+	char	**cmdstr;
+	char	*cmd;
+
+	cmdstr = ft_split(input, ' ');
+	if (cmdstr[0] == NULL)
+		exit(127);
+	if (ft_strichr(cmdstr[0], '/') > -1)
+		cmd = cmdstr[0];
+	else
+		cmd = get_path(cmdstr[0], environ);
+	execve(cmd, cmdstr, environ);
+	ft_putstr_fd("Error: Command not found (", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(")\n", STDERR_FILENO);
+	exit(127);
+} */
+
+void	execute_cmd(t_vars *vars)
+{
+	char	*cmd;
+
+	if (vars->token.tokens[0] == NULL)
+		exit(127);
+	if (ft_strichr(vars->token.tokens[0], '/') > -1)
+		cmd = vars->token.tokens[0];
+	else
+		cmd = get_path(vars->token.tokens[0], environ);
+	execve(cmd, vars->token.tokens, environ);
+	ft_putstr_fd("Error: Command not found (", STDERR_FILENO);
+	ft_putstr_fd(cmd, STDERR_FILENO);
+	ft_putstr_fd(")\n", STDERR_FILENO);
+	exit(127);
+}
+ 
+/* int	execute(t_vars *vars, char *input)
+{
+	pid_t	pid;
+
+	if (vars->built_in)
+		execute_built_in(vars, input);
+	else if (vars->pipe > 0)
+		execute_pipes(vars);
+	else
+	{
+		pid = fork();
+		if (pid == 0 && !vars->pipe)
+			execute_cmd(vars);
+		else if (pid != 0)
+			wait(&pid);
+	}
+	return (0);
+} */
+
+int	execute(t_vars *vars, char *input)
+{
+	pid_t	pid;
+
+	if (vars->pipe == 0)
+	{
+		if (vars->built_in)
+			execute_built_in(vars, input);
+		else
+		{
+			pid = fork();
+			if (pid == 0 && !vars->pipe)
+				execute_cmd(vars);
+			else if (pid != 0)
+				wait(&pid);
+		}
+	}
+	else if (vars->pipe > 0)
+		execute_pipes(vars);
+	return (0);
+}
